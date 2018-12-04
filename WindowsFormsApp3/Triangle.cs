bcfd4582bcffea659d4c008a763d1671d960ba63
Александр Краснов <sasha10101016@gmail.com>
2018-12-04 14:49:35 +0300

    Add Combobox and Timew

--- WindowsFormsApp3/Triangle.cs
+++ WindowsFormsApp3/Triangle.cs
@@ -2,51 +2,63 @@
 using System.Drawing;
 using System.Windows.Forms;
 
-
 namespace WindowsFormsApp3
 {
     class Triangle : Fractal
     {
-        //private int scaleIm = 1;
-        public int DepthInt { get; set; } 
-        public override float SideF { get; set; }
+        public const int correlation = 150;
+
+        private int maxDepthInt = 12;
         private float bx, cx;
         private float ay;
         private float bc_y;
-        private const int correlation = 150;
         private PointF top_point, right_point, left_point;
-        
-        public void Draw(Graphics g)
+
+        public override int DepthInt { get; set; }
+        public override int MaxDepthInt { get { return maxDepthInt; } set {maxDepthInt = value; } }
+        public override float SideF { get; set; }
+
+        public override void Draw(Graphics g)
         {
             DrawTriangle(g, DepthInt, top_point, left_point, right_point);
         }
-        public void CalculateInitialCoordinates()
+
+        internal void InitializeTriangle()
+        {
+            SideF = 128f;
+            StartColor = Color.Yellow;
+            EndColor = Color.Blue;
+        }
+
+        internal void CalculateInitialCoordinates()
         {
             CalculateInitialCoordinates(ref bx, ref cx, ref ay, ref bc_y);
         }
-        public void CalculatePoints()
+
+        internal void CalculatePoints()
         {
             CalculatePoints(ref top_point, ref right_point, ref left_point);
         }
-        protected override void DrawTriangle(Graphics gr, int level, PointF top_point, PointF left_point, PointF right_point)
+        
+        private void DrawTriangle(Graphics gr, int level, PointF top_point, PointF left_point, PointF right_point)
         {
             try
             {
-                Color colorCol = CountGradient.Gradient(startColor, endColor, DepthInt + 2, level);
+                Color colorCol = CountGradient.Gradient(StartColor, EndColor, DepthInt + 2, level);
                 Brush colorBrush = new SolidBrush(colorCol);
                 if (level == 0)
                 {
                     PointF[] points = { top_point, right_point, left_point };
-                    gr.FillPolygon(new SolidBrush(startColor), points);
+                    gr.FillPolygon(new SolidBrush(StartColor), points);
                 }
                 else
                 {
-                    // Найти граничные точки треугольника
+                    // Find the boundary points of the triangle
                     PointF left_mid = new PointF((top_point.X + left_point.X) / 2f, (top_point.Y + left_point.Y) / 2f);
                     PointF right_mid = new PointF((top_point.X + right_point.X) / 2f, (top_point.Y + right_point.Y) / 2f);
                     PointF bottom_mid = new PointF((left_point.X + right_point.X) / 2f, (left_point.Y + right_point.Y) / 2f);
-
-                    // Рекурсивно рисуем три меньших треугольника
+                    
+                    // Recursively draw three smaller triangles
                     DrawTriangle(gr, level - 1, right_mid, bottom_mid, right_point);
                     DrawTriangle(gr, level - 1, left_mid, left_point, bottom_mid);
                     DrawTriangle(gr, level - 1, top_point, left_mid, right_mid);
@@ -55,9 +67,11 @@ namespace WindowsFormsApp3
             }
             catch (StackOverflowException err)
             {
-                MessageBox.Show($"Невозможно построить! {err.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Невозможно построить! {err.Message}", 
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-        }       
+        }
+
         private void CalculateInitialCoordinates(ref float bx, ref float cx, ref float ay, ref float bc_y)
         {
             bx = 5;
@@ -65,6 +79,7 @@ namespace WindowsFormsApp3
             ay = 1;
             bc_y = ay + (float)Math.Sqrt(SideF * SideF - SideF * SideF / 4);
         }
+
         private void CalculatePoints(ref PointF top_point, ref PointF right_point, ref PointF left_point)
         {
             top_point = new PointF((cx + bx) / 2 * Form1.scaleIm, ay * Form1.scaleIm);
